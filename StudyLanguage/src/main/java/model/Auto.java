b898fb0b80876b062e65fb1a5c97183e6a494a9b
ErmolaevaAlina <gromkiyland@gmail.com>
2018-07-11 19:41:29 +0300

    commit

---
+++ StudyLanguage/src/main/java/model/Auto.java
@@ -0,0 +1,64 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Auto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String model;
+
+    //можно не указывать Column name, если оно совпадает с названием столбца в таблице
+    private String color;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private User user;
+
+    public Auto() {
+    }
+
+    public Auto(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + model;
+    }
+}
